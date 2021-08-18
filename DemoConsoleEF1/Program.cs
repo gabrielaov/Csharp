26a4d8cd7e876d4b34979920df745b58f183bfaa
gabrielaov <74983647+gabrielaov@users.noreply.github.com>
2021-08-18 16:49:46 -0300

    initial commit

---
+++ DemoConsoleEF1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoConsoleEF1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
